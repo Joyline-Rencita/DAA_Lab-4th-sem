a6417ed3ea32cfde4a7835fd01e8d53caceaafc7
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-07-23 20:43:23 +0530

    Create prog_02a.java

---
+++ prog_02a.java
@@ -0,0 +1,3 @@
+2a. Design a superclass called Staff with details as StaffId, Name, Phone, and Salary. Extend this class by writing three subclasses namely Teaching (domain, publications), Technical (skills), and Contract (period). 
+Write a Java program to read and display at least 3 staff objects of all three categories.
+
